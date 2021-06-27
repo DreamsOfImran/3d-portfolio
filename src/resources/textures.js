24937c8c705b14b9bf5e7c8b6ecda07b2769f3d7
Imran Basha <ibasha66@gmail.com>
2021-06-27 13:05:38 +0530

    Initial commit for my 3D Portfolio

---
+++ src/resources/textures.js
@@ -0,0 +1,61 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.grpcBlogTexture = '../src/jsm/grpc_pic.jpeg';
+billboardTextures.webauthnBlogTexture = '../src/jsm/webauthn_pic.png';
+
+//box textures
+let boxTexture = {};
+boxTexture.allSkills = '../src/jsm/mySkills.png';
+boxTexture.aboutMe = '../src/jsm/about_me.png';
+
+boxTexture.facebook = '../src/jsm/facebook.png';
+boxTexture.twitter = '../src/jsm/twitter.png';
+boxTexture.instagram = '../src/jsm/instagram.png';
+boxTexture.github = '../src/jsm/github.png';
+boxTexture.medium = '../src/jsm/medium.png'
+boxTexture.devTo = '../src/jsm/dev.png';
+boxTexture.linkedIn = '../src/jsm/linkedInLogo.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.thunder = '../src/jsm/thunder.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+
+//material textures
+let stoneTexture = '../src/jsm/stone.png';
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+inputText.grpcBlogText = '../src/jsm/grpc_txt.png';
+inputText.timeline = '../src/jsm/timeline.png';
+inputText.webauthnBlogText = '../src/jsm/webauthn_txt.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.grpcBlog = 'https://medium.com/@DreamsOfImran/learn-grpc-and-protobuf-with-golang-8456a2e64977';
+URL.webauthnBlog = 'https://medium.com/@DreamsOfImran/passwordless-authentication-for-web-app-using-webauthn-162165692636';
+
+URL.facebook = 'https://facebook.com/DreamsOfImran';
+URL.twitter = 'https://twitter.com/DreamsOfImran';
+URL.instagram = 'https://instagram.com/dreamsofimran';
+URL.github = 'https://github.com/DreamsOfImran';
+URL.medium = 'https://medium.com/@DreamsOfImran'
+URL.devTo = 'https://dev.to/dreamsofimran';
+URL.linkedIn = 'https://www.linkedin.com/in/imran-basha/';
+URL.imranbasha = 'https://dreamsofimran.in';
+URL.email = 'mailto:mail@dreamsofimran.in';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
